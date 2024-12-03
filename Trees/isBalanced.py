cda0b8bf9107696272958b6583cd6677d84b0b35
Splavacodo <splavacodo56@gmail.com>
2024-12-02 22:45:57 -0700

    Revision of Leetcode Prob. 110

--- Trees/isBalanced.py
+++ Trees/isBalanced.py
@@ -8,20 +8,43 @@ class TreeNode:
         self.left = left
         self.right = right
 
-def isBalanced(root: TreeNode) -> bool: 
-    def dfs(node: TreeNode) -> int: 
-        nonlocal balanced
-        if not node: 
-            return 0
+# def isBalanced(root: TreeNode) -> bool: 
+#     def dfs(node: TreeNode) -> int: 
+#         nonlocal balanced
+#         if not node: 
+#             return 0
         
-        leftHeight = dfs(node.left)
-        rightHeight = dfs(node.right)
+#         leftHeight = dfs(node.left)
+#         rightHeight = dfs(node.right)
 
-        if abs(leftHeight - rightHeight) > 1: 
-            balanced = False 
+#         if abs(leftHeight - rightHeight) > 1: 
+#             balanced = False 
         
-        return 1 + max(leftHeight, rightHeight)
+#         return 1 + max(leftHeight, rightHeight)
  
-    balanced = True
-    dfs(root)
-    return balanced
+#     balanced = True
+#     dfs(root)
+#     return balanced
+
+class Solution:
+    def isBalanced(self, root: TreeNode) -> bool:
+        """
+        Reattempt at isBalanced
+
+        Date: 12/02/24
+        """
+        def get_height(node):
+            if node is None:
+                return 0
+            return max(get_height(node.left), get_height(node.right)) + 1
+        
+        if root is None:
+            return True
+        
+        left_height = get_height(root.left)
+        right_height = get_height(root.right)
+
+        if abs(right_height - left_height) > 1:
+            return False
+
+        return self.isBalanced(root.left) and self.isBalanced(root.right)
