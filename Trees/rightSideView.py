4ac504e93c455dbd05f343828362ff96b80ca58c
Splav <splavacodo56@gmail.com>
2024-12-10 19:39:30 -0700

    Solved Leetcode prob. 199

---
+++ Trees/rightSideView.py
@@ -0,0 +1,33 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def rightSideView(self, root: TreeNode) -> list[int]:
+        levels = self.level_order(root)
+
+        visible = []
+        for level in levels:
+            visible.append(level[-1])
+        
+        return visible
+            
+    
+    def level_order(self, root: TreeNode) -> list[list[int]]:
+        level_order = []
+        self.visit(root, 0, level_order)
+
+        return level_order
+
+    def visit(self, node, level, node_list):
+        if not node:
+            return
+        
+        if len(node_list) <= level:
+            node_list.append([])
+        
+        node_list[level].append(node.val)
+        self.visit(node.left, level + 1, node_list)
+        self.visit(node.right, level + 1, node_list)
